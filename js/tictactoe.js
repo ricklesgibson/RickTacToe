83818d6702ff5234f52e47f67f41afb8910164ac
ricklesgibson <ricklesgibson@gmail.com>
2015-02-01 21:21:13 -0800

    Cleaned up visual styles. Still needs work on look and firebase logic

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -8,71 +8,72 @@ app.controller("TicTacCtrl", function($scope, $firebase){
   var sync = $firebase(ref);
   $scope.remoteGameContainer = sync.$asObject();
 
+
+//array of object to give board cells ownership and point values
 $scope.board = 
        [{status:"Blank", pt:1}, {status:"Blank", pt:2},  {status:"Blank", pt:4},
         {status:"Blank", pt:8}, {status:"Blank", pt:16}, {status:"Blank", pt:32},
         {status:"Blank", pt:64},{status:"Blank", pt:128},{status:"Blank", pt:256}];
 
 // init variables
-$scope.gameSta="Game On!";
+$scope.gameStatus="Game On!";
 $scope.moveCounter = 0;
 $scope.p1Point = 0;
 $scope.p2Point = 0;
-$scope.wsWinCount = 0;
-$scope.bsWinCount = 0;
-$scope.playFirst ="";
-$scope.playSecond ="";
-$scope.whoseTurn = 'X';
+$scope.leftWinCount = 0;
+$scope.rightWinCount = 0;
+$scope.playFirst = "";
+$scope.playSecond = "";
 
-// Special Sauce. My local stuffs that I want to load to Firebase
+
+// Sync this object to firebase.
 $scope.gameContainer = {
   boardArray: $scope.board,
-  gameSta: $scope.gameSta,      // $scope.gameContainer.gameSta ~ $scope.gameStatus
+  gameStatus: $scope.gameStatus,
   moveCount: $scope.moveCounter,
   p1Pt: $scope.p1Point,
   p2Pt: $scope.p2Point,
-  wsWinC: $scope.wsWinCount,
-  bsWinC: $scope.bsWinCount,
+  leftWinC: $scope.leftWinCount,
+  rightWinC: $scope.rightWinCount,
   play1st: $scope.playFirst,
   play2nd: $scope.playSecond,
   notification: "",
-  turn: 0
 };
 
-$scope.clearData = function(){ $scope.gameContainer = {};}
+$scope.clearData = function(){ $scope.gameContainer = {};} // clear Firebase data
 
 $scope.gameStart = function(){                        // Restart game
   $scope.gameContainer.boardArray =   [{status:"Blank", pt:1}, {status:"Blank", pt:2},  {status:"Blank", pt:4},
                     {status:"Blank", pt:8}, {status:"Blank", pt:16}, {status:"Blank", pt:32},
                     {status:"Blank", pt:64},{status:"Blank", pt:128},{status:"Blank", pt:256}];
   // init variables
-  $scope.gameContainer.gameSta="Game On!";
+  $scope.gameContainer.gameStatus="Game On!";
   $scope.gameContainer.moveCount = 0;
   $scope.gameContainer.p1Pt = 0;
   $scope.gameContainer.p2Pt = 0;
   $scope.showLeft = "";
   $scope.showRight= "";
-  $scope.gameContainer.wsWinC = $scope.wsWinCount;
-  $scope.gameContainer.bsWinC = $scope.bsWinCount;
+  $scope.gameContainer.leftWinC = $scope.leftWinCount;
+  $scope.gameContainer.rightWinC = $scope.rightWinCount;
   
 };
 
 
-// Angular stuffs. Connect my local stuffs to/from Firebase
+// Three-way binding with Firebase, throws status message when synced object is changed.
 $scope.remoteGameContainer.$bindTo($scope,  "gameContainer");
 $scope.$watch('gameContainer', function(){
 console.log('gameContainer changed!');
-console.log($scope.gameSta);
+console.log($scope.gameStatus);
 console.log($scope.gameContainer);
-console.log($scope.wsWinCount);
+console.log($scope.leftWinCount);
 
 });
 
 
 
-// function Starts
-$scope.playerPicks = function(thisCell){                    // function playerPicks starts 
-  if ((thisCell.status == "Blank") && ($scope.gameContainer.gameSta=="Game On!")) {   // check for blank cells and game status is on.
+// gameplay function
+$scope.playerPicks = function(thisCell){                   // function playerPicks starts 
+  if ((thisCell.status == "Blank") && ($scope.gameContainer.gameStatus=="Game On!")) {   // check for blank cells and game status is on.
     $scope.gameContainer.moveCount = $scope.gameContainer.moveCount + 1;  // increase move counter
     console.log($scope.gameContainer.moveCount);  
     // Change whose turn it is
@@ -87,7 +88,6 @@ $scope.playerPicks = function(thisCell){                    // function playerPi
         thisCell.status = $scope.gameContainer.play2nd;
         $scope.gameContainer.p2Pt = $scope.gameContainer.p2Pt + thisCell.pt ;         // increase P2 points.
       } ;
-      console.log("hi");
 
       var winPoint = [7,56,73,84,146,273,292,448];            // 8 possible winning points in array.
 
@@ -113,7 +113,7 @@ $scope.playerPicks = function(thisCell){                    // function playerPi
 
 
 
-
+//selects which character moves first
 $scope.currentPlayer = function(who){                     // Determine left or right side play first.
   if ((who=="left") && ($scope.gameContainer.moveCount == 0)) {
     $scope.gameContainer.play1st ="W";
@@ -131,30 +131,30 @@ $scope.currentPlayer = function(who){                     // Determine left or r
 
 
 $scope.firstPlayerWin = function(){                       // First player won
-  $scope.gameContainer.gameSta="First Player Wins!";
+  $scope.gameContainer.gameStatus="First Player Wins!";
     if ($scope.gameContainer.play1st == "W") {
       $scope.showLeft = "White Spy Wins!";
-      $scope.gameContainer.wsWinC = $scope.gameContainer.wsWinC + 1;
-      console.log($scope.gameContainer.wsWinC);
+      $scope.gameContainer.leftWinC = $scope.gameContainer.leftWinC + 1;
+      console.log($scope.gameContainer.leftWinC);
     } else if ($scope.gameContainer.play1st == "B") {
       $scope.showRight = "Black Spy Wins!";
-      $scope.gameContainer.bsWinC ++;
+      $scope.gameContainer.rightWinC ++;
     };
 };
 
 $scope.secondPlayerWin = function(){                      // Second player won
-  $scope.gameContainer.gameSta="Second Player Wins!";
+  $scope.gameContainer.gameStatus="Second Player Wins!";
     if ($scope.gameContainer.play2nd == "W") {
       $scope.showLeft = "White Spy Wins!";
-      $scope.gameContainer.wsWinC = $scope.gameContainer.wsWinC + 1;
+      $scope.gameContainer.leftWinC = $scope.gameContainer.leftWinC + 1;
     } else if ($scope.gameContainer.play2nd == "B") {
       $scope.showRight = "Black Spy Wins!";
-      $scope.gameContainer.bsWinC ++;
+      $scope.gameContainer.rightWinC ++;
     };
 };
 
 $scope.gameOver = function(){                         // Game Over function
-  $scope.gameContainer.gameSta="TIE! Game Over!";
+  $scope.gameContainer.gameStatus="TIE! Game Over!";
   $scope.showLeft = "It's a tie";
   $scope.showRight = "It's a tie";
   console.log("tie")
@@ -165,7 +165,7 @@ $scope.gameRestart = function(){                        // Restart game
                     {status:"Blank", pt:8}, {status:"Blank", pt:16}, {status:"Blank", pt:32},
                     {status:"Blank", pt:64},{status:"Blank", pt:128},{status:"Blank", pt:256}];
   // init variables
-  $scope.gameContainer.gameSta="Game On!";
+  $scope.gameContainer.gameStatus="Game On!";
   $scope.gameContainer.moveCount = 0;
   $scope.gameContainer.p1Pt = 0;
   $scope.gameContainer.p2Pt = 0;
