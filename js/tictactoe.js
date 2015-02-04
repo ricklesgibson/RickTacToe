7e0889f9bfa6023de9ecc9187757b2b6a1fa0aa5
ricklesgibson <ricklesgibson@gmail.com>
2015-02-03 20:54:36 -0800

    Added more functionality with Lorin, still have sync bug

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,5 +1,3 @@
-
-
 var app = angular.module("TicTacApp", ["firebase"]);
 
 app.controller("TicTacCtrl", function($scope, $firebase){
@@ -10,11 +8,7 @@ app.controller("TicTacCtrl", function($scope, $firebase){
 
 
 //array of object to give board cells ownership and point values
-$scope.board = 
-       [{status:"Blank", pt:1}, {status:"Blank", pt:2},  {status:"Blank", pt:4},
-        {status:"Blank", pt:8}, {status:"Blank", pt:16}, {status:"Blank", pt:32},
-        {status:"Blank", pt:64},{status:"Blank", pt:128},{status:"Blank", pt:256}];
-
+       
 // init variables
 $scope.gameStatus="Game On!";
 $scope.moveCounter = 0;
@@ -29,7 +23,9 @@ $scope.playSecond = "";
 
 // Sync this object to firebase.
 $scope.gameContainer = {
-  boardArray: $scope.board,
+  boardArray: [{status:"Blank", pt:1}, {status:"Blank", pt:2},  {status:"Blank", pt:4},
+        {status:"Blank", pt:8}, {status:"Blank", pt:16}, {status:"Blank", pt:32},
+        {status:"Blank", pt:64},{status:"Blank", pt:128},{status:"Blank", pt:256}],
   gameStatus: $scope.gameStatus,
   moveCount: $scope.moveCounter,
   p1Pt: $scope.p1Point,
@@ -71,41 +67,44 @@ console.log($scope.leftWinCount);
 });
 
 
-
+var playerWon = false;
 // gameplay function
-$scope.playerPicks = function(thisCell){                   // function playerPicks starts 
+$scope.playerPicks = function(thisCell){ 
+                 // function playerPicks starts 
   if ((thisCell.status == "Blank") && ($scope.gameContainer.gameStatus=="Game On!")) {   // check for blank cells and game status is on.
       if (($scope.gameContainer.moveCount % 2) == 0){  
-        $scope.gameContainer.moveCount++  // increase move counter
         console.log($scope.gameContainer.moveCount);           // if move counter is odd, then it's P1 move.
         thisCell.status = $scope.gameContainer.play1st;
         $scope.gameContainer.p1Pt = $scope.gameContainer.p1Pt + thisCell.pt ;         // increase P1 points.
       } else if (($scope.gameContainer.moveCount % 2) != 0){        // if move counter is even, then it's P2 move.
-        $scope.gameContainer.moveCount++  // increase move counter
         console.log($scope.gameContainer.moveCount); 
         thisCell.status = $scope.gameContainer.play2nd;
         $scope.gameContainer.p2Pt = $scope.gameContainer.p2Pt + thisCell.pt ;         // increase P2 points.
-      } ;
+      } 
+      $scope.gameContainer.moveCount++ ; // Inc. move count
 
       var winPoint = [7,56,73,84,146,273,292,448];            // 8 possible winning points in array.
 
       for (var i=0; i<8; i++){                      // The Win Logic!
         if ((winPoint[i] & $scope.gameContainer.p1Pt) == winPoint[i]){        // Binary check winPoint & first player point
-          console.log("p1 win. from For loop.")           // If binary of winPoint & p1Point = winPoint
-          $scope.firstPlayerWin();                        // fire the xWin function
-        };                                
+          console.log("p1 win. from For loop.");         // If binary of winPoint & p1Point = winPoint
+          $scope.firstPlayerWin();  
+          playerWon = true;                      // fire the xWin function
+        }                               
         if ((winPoint[i] & $scope.gameContainer.p2Pt) == winPoint[i]){        // Binary check winPoint & second player point
-          console.log("p2 win. from For loop.")           // If binary of winPoint & p2Point = winPoint
-          $scope.secondPlayerWin();                       // fire the oWin function
-        };
-      };
+          console.log("p2 win. from For loop.");          // If binary of winPoint & p2Point = winPoint
+          $scope.secondPlayerWin();  
+          playerWon = true;                     // fire the oWin function
+        }
+      }
 
-  }; // end if
-      if ($scope.gameContainer.moveCount == 9) {              // Fire game-over function when max move is reached
+    
+    
+      if (!playerWon && $scope.gameContainer.moveCount == 9) {              // Fire game-over function when max move is reached
         $scope.gameOver();
-        console.log("theend!")
+        console.log("theend!");
       }; // end if
-
+  } 
 
 }; // function playerPicks ends
 
